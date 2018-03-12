9023e9ab2c2da5ba1aa92d59c1b2a65a94c46008
Fabian Tjoe-A-On <fabiantjoeaon@gmail.com>
2018-03-12 16:09:14 +0100

    moved set to mixcloud

--- src/pages/index.js
+++ src/pages/index.js
@@ -321,6 +321,8 @@ class IndexPage extends Component {
                         <br />
                             <iframe width="100%" height="60" src="https://www.mixcloud.com/widget/iframe/?hide_cover=1&mini=1&feed=%2Ffabian-tjoe-a-on%2Fobscurityelation%2F" frameborder="0" ></iframe>
                         <br />
+                        <iframe width="100%" height="60" src="https://www.mixcloud.com/widget/iframe/?hide_cover=1&mini=1&feed=%2Ffabian-tjoe-a-on%2Flate_night_session%2F" frameborder="0" ></iframe>
+                        <br />
                     </StyledProjectWrapper>
                 </Content>{' '}
             </IndexContainer>
