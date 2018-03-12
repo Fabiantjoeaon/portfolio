e82438bd96525f72dab9c9d18029e7caf30d2a86
Fabian Tjoe-A-On <fabiantjoeaon@gmail.com>
2018-03-12 16:09:55 +0100

    break

--- src/pages/index.js
+++ src/pages/index.js
@@ -319,6 +319,7 @@ class IndexPage extends Component {
                     <StyledProjectWrapper>
                         <StyledProjectTitle>Recordings</StyledProjectTitle>{' '}
                         <br />
+                        <br />
                             <iframe width="100%" height="60" src="https://www.mixcloud.com/widget/iframe/?hide_cover=1&mini=1&feed=%2Ffabian-tjoe-a-on%2Fobscurityelation%2F" frameborder="0" ></iframe>
                         <br />
                         <iframe width="100%" height="60" src="https://www.mixcloud.com/widget/iframe/?hide_cover=1&mini=1&feed=%2Ffabian-tjoe-a-on%2Flate_night_session%2F" frameborder="0" ></iframe>
