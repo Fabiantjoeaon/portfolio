ab84986901e87ffe2c36b69186bab97f21e7cc8d
Fabian Tjoe-A-On <fabiantjoeaon@gmail.com>
2018-03-12 15:17:47 +0100

    gatsby-plugin-manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,33 @@ module.exports = {
         themeColor: '#a5abff'
     },
     plugins: [
+        {
+            resolve: `gatsby-plugin-manifest`,
+            options: {
+              name: "Fabian Tjoe-A-On",
+              short_name: "FT",
+              start_url: "/",
+              background_color: "#fff",
+              theme_color: "#a5abff",
+              display: "minimal-ui",
+              icons: [
+                {
+                  // Everything in /static will be copied to an equivalent
+                  // directory in /public during development and build, so
+                  // assuming your favicons are in /static/favicons,
+                  // you can reference them here
+                  src: `/favicons/android-chrome-192x192.png`,
+                  sizes: `192x192`,
+                  type: `image/png`,
+                },
+                {
+                  src: `/favicons/android-chrome-512x512.png`,
+                  sizes: `512x512`,
+                  type: `image/png`,
+                },
+              ],
+            },
+          },
         'gatsby-plugin-react-helmet',
         {
             resolve: `gatsby-plugin-typography`,
